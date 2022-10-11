811338e0d095a417d2ddfe37f55f5847af553c7b
Anshu <priyanshuupadhyay>
2022-10-11 14:29:00 +0530

    project 1

---
+++ Day_16/Movie_Recommander.py
@@ -0,0 +1,60 @@
+
+import tkinter as ttk
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+import os
+print('Location is:',os.getcwd(),'\n\n\n')
+app = ttk.Tk()
+app.title('Recommandation System')
+app.geometry('500x400')
+cols = ['user_id', 'movie_id', 'rating', 'ts']
+df = pd.read_csv('u.data', sep = '\t', names = cols).drop('ts', axis = 1)
+item_cols = ['movie_id', 'title'] + [str(i) for i in range(22)]
+df1 = pd.read_csv('u.item', sep = '|', names = item_cols, encoding = "ISO-8859-1")[['movie_id', 'title']]
+movie = pd.merge(df, df1, on = 'movie_id')
+
+
+result = ttk.Variable(app)
+
+frame = ttk.Frame(app)
+frame.place(x=10,y=10)
+box = ttk.Listbox(frame, height = 10 , width=50)
+for title in movie['title'].unique():
+    box.insert(ttk.END,title)
+
+box.pack(side='left', fill = 'y')
+
+#box.place(x = 10 ,y =10)
+
+scroll = ttk.Scrollbar(frame,orient=ttk.VERTICAL)
+scroll.config(command=box.yview)
+box.config(yscrollcommand=scroll.set)
+scroll.pack(side = 'right' , fill = 'y')
+
+
+def get_movie():
+    movie_selected = box.get(box.curselection())
+    print('movie_selected:',movie_selected)
+
+    
+    movie_pivot = movie.pivot_table(index = 'user_id', columns = 'title', values = 'rating')
+    
+    corrs = movie_pivot.corrwith(movie_pivot[movie_selected])
+    corrs_df = pd.DataFrame(corrs, columns = ['correlation'])
+    corrs_df['avg_rating'] = movie.groupby('title')['rating'].mean()
+    corrs_df['count'] = movie['title'].value_counts()
+    
+    
+    top_recom = list(corrs_df[corrs_df['count'] > 50].sort_values(by = 'correlation', ascending = False).head(3).index)
+    if movie_selected in top_recom:
+       top_recom.remove(movie_selected)
+    print('Recommandation:',top_recom)
+    if top_recom:
+       result.set(top_recom[0])
+    else:
+        result.set('Sorry No Recommandation Found!!')
+ttk.Button(app , text = 'Find Recommandation', font = ('Arial' ,12), command = get_movie).place(x= 10 , y =250)
+ttk.Label(app , textvariable=result, font = ('Arial',22)).place(x = 200 , y = 200)
+
+app.mainloop()
